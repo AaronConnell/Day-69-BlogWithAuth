9c7acabcfad5c55f839f65076107857c3d1d55a8
aaron <aaron@con-man.net>
2023-01-19 11:36:44 -0700

    Update environment vars

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from flask import Flask, render_template, redirect, url_for, flash, abort
 from flask_bootstrap import Bootstrap
 from flask_ckeditor import CKEditor
 from datetime import date
+import os
 
 from sqlalchemy import Integer, ForeignKey
 from werkzeug.security import generate_password_hash, check_password_hash
@@ -15,12 +16,11 @@ from flask_gravatar import Gravatar
 
 login_manager = LoginManager()
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
 ckeditor = CKEditor(app)
 Bootstrap(app)
 
 # CONNECT TO DB
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
